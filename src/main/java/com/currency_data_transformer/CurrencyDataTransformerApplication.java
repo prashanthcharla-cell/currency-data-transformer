3309163ff407aaeb1b5502953c642a7fc8bb08bc
Prashanth Charla <prashanthcharla1234@gmail.com>
2026-01-06 18:46:20 +0530

    Initial commit

---
+++ src/main/java/com/currency_data_transformer/CurrencyDataTransformerApplication.java
@@ -0,0 +1,13 @@
+package com.currency_data_transformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyDataTransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyDataTransformerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/currency_data_transformer/CurrencyDataTransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currency_data_transformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyDataTransformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
